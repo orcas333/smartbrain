b8749c3b3d7783ac2b2109ca9270c8d295d51e11
Jenny Hu <orcas03030@gmail.com>
2020-10-20 15:22:30 -0700

    fixing Register component

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -55,4 +55,4 @@ const SignIn = ({ onRouteChange }) => {
   );
 };
 
-export default SignIn;
+export default Register;
