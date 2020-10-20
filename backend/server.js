cb2ac0f17cb71d1e25c4c240fa0feafb5a88bf6c
Jenny Hu <orcas03030@gmail.com>
2020-10-20 14:54:37 -0700

    linking frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,116 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
-app.use(express.static(__dirname + "/public"));
 
-app.listen(3000);
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: "987",
+      hash: "",
+      email: "john@gmail.com"
+    }
+  ]
+};
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  bcrypt.compare(
+    "apples",
+    "$2a$10$phx1LvO0paJ3okA/BkI5xeQOTP4xch.lmMKCNhmSHs/g/vLz4zsdm",
+    function(err, res) {
+      console.log("first guess", res);
+    }
+  );
+  bcrypt.compare(
+    "veggies",
+    "$2a$10$phx1LvO0paJ3okA/BkI5xeQOTP4xch.lmMKCNhmSHs/g/vLz4zsdm",
+    function(err, res) {
+      console.log("second guess", res);
+    }
+  );
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    // Store hash in your password DB
+    console.log(hash);
+  });
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
