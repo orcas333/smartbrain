d50cdeb9c2dc7ddd3a957b4af2683c64e72d7953
Jenny Hu <orcas03030@gmail.com>
2020-10-22 16:26:10 -0700

    linked backend with a postgres database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "xaipei",
+    password: "",
+    database: "smartbrain"
+  }
+});
+
+db.select("*")
+  .from("users")
+  .then(data => {
+    console.log(data);
+  });
 
 const app = express();
 
