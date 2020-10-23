450f67f322af4d2bcd63eae4407bb65f12b62fe2
Jenny Hu <orcas03030@gmail.com>
2020-10-22 17:50:14 -0700

    finished signin endpoint and hashing passwords

--- backend/server.js
+++ backend/server.js
@@ -52,28 +52,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  bcrypt.compare(
-    "apples",
-    "$2a$10$phx1LvO0paJ3okA/BkI5xeQOTP4xch.lmMKCNhmSHs/g/vLz4zsdm",
-    function(err, res) {
-      console.log("first guess", res);
-    }
-  );
-  bcrypt.compare(
-    "veggies",
-    "$2a$10$phx1LvO0paJ3okA/BkI5xeQOTP4xch.lmMKCNhmSHs/g/vLz4zsdm",
-    function(err, res) {
-      console.log("second guess", res);
-    }
-  );
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
